8580c4448145d12550d1faf80da77233b0fb2e95
Amaani2024 <amanifathi58@gmail.com>
2025-12-16 23:08:40 +0530

    Initial project upload

---
+++ clothstorefrontend/src/shop.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 classname='text=-3xl front-bold underline'>Clothwebstore</h1>
+    </div>
+  );
+}
+
+export default App;
